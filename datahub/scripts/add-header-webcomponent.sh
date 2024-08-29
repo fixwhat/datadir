c55625a728a4b58045ad61e18590ccb93c1dcdfb
fixwhat <iso6400br@gmail.com>
2024-08-29 11:06:08 -0300

    Update add-header-webcomponent.sh

--- datahub/scripts/add-header-webcomponent.sh
+++ datahub/scripts/add-header-webcomponent.sh
@@ -4,7 +4,7 @@
 # so you need to add it with chmod and commit/push it
 
 DATAHUB_DIR=${1:-/usr/share/nginx/html/datahub}
-SNIPPET="<script src='${HEADER_SCRIPT}'></script><geor-header active-app='datahub' stylesheet='${georchestraStylesheet}' ></geor-header>"
+SNIPPET="<script src='${HEADER_SCRIPT}'></script><geor-header active-app='datahub' stylesheet='${GEO_STYLESHEET}' logo-url='${LOGO_URL}'></geor-header>"
 
 if grep -q "${SNIPPET}" "${DATAHUB_DIR}/index.html"; then
   echo "[INFO] geOrchestra: header already present."
